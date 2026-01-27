7b3c48d8b032044ef882eae6e17b9819b70720a1
Webprojeto <github@webprojeto.com.br>
2026-01-27 12:24:11 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,7 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Routes, Route } from "react-router-dom";
-import { LanguageProvider } from "@/contexts/LanguageContext";
-
-import Index from "@/pages/Index";
-import TermsOfUse from "@/pages/TermsOfUse";
-import NotFound from "@/pages/NotFound";
-
-const queryClient = new QueryClient();
-
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <LanguageProvider>
-        <Toaster />
-        <Sonner />
-
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/terms" element={<TermsOfUse />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-
-      </LanguageProvider>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
-
-export default App;
+<BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Index />} />
+    <Route path="/terms" element={<TermsOfUse />} />
+    <Route path="*" element={<NotFound />} />
+  </Routes>
+</BrowserRouter>
