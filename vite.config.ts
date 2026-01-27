7e4cc537bea73cafa53adfa35ae06cd60d983169
Webprojeto <github@webprojeto.com.br>
2026-01-27 12:02:28 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-
 export default defineConfig({
+  base: "/webprojeto-preceptor/",
   plugins: [react()],
   resolve: {
     alias: {
