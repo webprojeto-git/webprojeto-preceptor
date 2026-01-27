ee0f714c9a2bec7d45866266012a11f6e65e1d14
Webprojeto <github@webprojeto.com.br>
2026-01-27 12:02:02 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,14 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { LanguageProvider } from "@/contexts/LanguageContext";
+
 import Index from "@/pages/Index";
 import TermsOfUse from "@/pages/TermsOfUse";
 import NotFound from "@/pages/NotFound";
 
 const queryClient = new QueryClient();
-
-const basename =
-  typeof window !== "undefined" &&
-  window.location.pathname.startsWith("/webprojeto-preceptor")
-    ? "/webprojeto-preceptor"
-    : import.meta.env.PROD
-      ? "/webprojeto-preceptor"
-      : "";
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
@@ -24,13 +17,13 @@ const App = () => (
       <LanguageProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter basename={basename}>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/terms" element={<TermsOfUse />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
+
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/terms" element={<TermsOfUse />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+
       </LanguageProvider>
     </TooltipProvider>
   </QueryClientProvider>
