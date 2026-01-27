8faaa1a41b3856978b0f349629bcba258866bf0c
Webprojeto <github@webprojeto.com.br>
2026-01-27 11:32:08 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
 
 export default defineConfig({
   base: "/",
-  server: {
-    port: 8080,
-  },
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
 });
